74054d36f7f541ada63e6fc8567f2a66c10a0aa5
CarlRWarren <cwarren@student.neumont.edu>
2019-03-24 18:38:17 -0600

    finals

--- KeepItUp/Assets/Scripts/Ball.cs
+++ KeepItUp/Assets/Scripts/Ball.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private void Start()
+    {
+        transform.position = new Vector3(Random.Range(-3.0f, 8.0f), Random.Range(12.0f, 25.0f), Random.Range(-2.0f, 12.0f));
+    }
     private void Update()
     {
         if (transform.position.y < 0.0f)
---
+++ KeepItUp/Assets/Scripts/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class Menu : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI m_highScoreText = null;
+    [SerializeField] TextMeshProUGUI m_scoreText = null;
+
+    int m_score = 0;
+    int m_highscore = 0;
+    int count = 0;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            m_highscore = PlayerPrefs.GetInt("HighScore");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", m_highscore);
+        }
+
+        m_highScoreText.text = m_highscore.ToString("D6");
+        
+    }
+
+    private void Update()
+    {
+        count++;
+        if (count > 20)
+        {
+            count = 0;
+            m_score++;
+            if (m_score > m_highscore)
+            {
+                m_highscore = m_score;
+                PlayerPrefs.SetInt("HighScore", m_highscore);
+                m_highScoreText.text = m_highscore.ToString("D6");
+            }
+            m_scoreText.text = m_score.ToString("D6");
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+}
